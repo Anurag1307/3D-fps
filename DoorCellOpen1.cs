0bcf4f05c7bde1fa3bebf9bccf7125db6a53dc53
Anurag1307 <42073204+Anurag1307@users.noreply.github.com>
2018-08-12 22:01:59 +0530

    Add files via upload

---
+++ DoorCellOpen1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class DoorCellOpen1 : MonoBehaviour {
+
+	public float Distance;
+	public GameObject ActionDisplay;
+	public GameObject ActionText;
+	public GameObject Door;
+
+
+
+	void Update () {
+		Distance = rayCasting.DistanceFromTarget;
+	}
+
+	void OnMouseOver () {
+		if (Distance <= 2) {
+			ActionDisplay.SetActive (true);
+			ActionText.SetActive (true);
+		}
+		if (Input.GetButtonDown("action")) {
+			if (Distance <= 2) {
+				this.GetComponent<BoxCollider>().enabled = false;
+				ActionDisplay.SetActive(false);
+				ActionText.SetActive(false);
+				Door.GetComponent<Animation> ().Play ("FenceDoorLeft");
+
+
+			}
+		}
+	}
+
+	void OnMouseExit() {
+		ActionDisplay.SetActive (false);
+		ActionText.SetActive (false);
+	}
+}
