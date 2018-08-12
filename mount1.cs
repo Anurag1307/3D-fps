77826d113012d1a91fa66a608224d2c19a7ef0cd
Anurag1307 <42073204+Anurag1307@users.noreply.github.com>
2018-08-12 22:31:25 +0530

    Add files via upload

---
+++ mount1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mount1 : MonoBehaviour {
+    public GameObject objective002;
+    public GameObject objective003;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        objective002.SetActive(false);
+        objective003.SetActive(true);
+    }
+}
